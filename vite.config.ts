3c287b882409a53a5263e7bfbd7ea5f01bf0c4a7
Seyyed Ali Mohammadiyeh (MAX BASE) <MaxBaseCode@gmail.com>
2026-06-10 12:32:16 +0330

    deploy in a dir

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "node:path";
 
 export default defineConfig({
+  base: "/KashanLug/",
   plugins: [tailwindcss()],
   resolve: {
     alias: { "@": resolve(__dirname, "src") },
